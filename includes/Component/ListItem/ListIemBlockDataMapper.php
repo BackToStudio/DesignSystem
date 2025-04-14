5088d5b0b11f18afc546732c084ecea0d6f6e70d
Maïté Armengaud <53378428+maitearmengaud@users.noreply.github.com>
2025-04-14 18:06:47 +0200

    feat: ListItemComponent

---
+++ includes/Component/ListItem/ListIemBlockDataMapper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BackTo\DesignSystem\BlockEditor\BlockDataMapper;
+
+use BackTo\DesignSystem\Component\ListItem\ListItemComponent;
+use BackTo\DesignSystem\Component\TokenComponent;
+use WP_Block;
+
+class ListItemBlockDataMapper extends BlockDataMapper
+{
+    const BLOCK_NAME = 'core/list-item';
+    private ListItemDecorator $listItemDecorator;
+
+    public function __construct(
+        ListItemDecorator $listItemDecorator
+    ){
+        $this->listItemDecorator = $listItemDecorator;
+    }
+
+    public function getComponent(array $block): TokenComponent
+    {
+        return new ListItemComponent();
+    }
+
+    public function applyData($component, string $blockContent, array $block, WP_Block $instance): void {
+        /** @var ListItemComponent $component */
+        // TODO : add real content
+        $component->addChild('Fake content');
+    }
+
+    public function applyStyles($component, string $blockContent, array $block, WP_Block $instance): void {
+        /** @var ListItemComponent $component */
+        $component->addDecorator($this->listItemDecorator);
+
+    
+        if (isset($block['attrs']['className'])) {
+            $component->addClass($block['attrs']['className']);
+        }
+    }
+}
---
+++ includes/Component/ListItem/ListItemComponent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BackTo\DesignSystem\Component\ListItem;
+
+use BackTo\DesignSystem\Component\TokenComponent;
+
+class ListItemComponent extends TokenComponent
+{
+    protected string $tagName = 'li';
+
+}
