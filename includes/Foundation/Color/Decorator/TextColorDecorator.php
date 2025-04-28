d33f11b432dd0d8f6c7185bd8ed03a96667a347f
Florian Truchot <florian@truchot.co>
2025-04-28 11:53:23 +0200

    feat(color): update TextColorDecorator for improved functionality

--- includes/Foundation/Color/Decorator/TextColorDecorator.php
+++ includes/Foundation/Color/Decorator/TextColorDecorator.php
@@ -4,17 +4,19 @@ namespace BackTo\DesignSystem\Foundation\Color\Decorator;
 
 use BackTo\DesignSystem\Contracts\StyleDecorator;
 use BackTo\DesignSystem\Foundation\Color\Decorator\ColorDecorator;
+use DiplomaEducation\Foundation\Color\Configurator\TextColorConfigurator;
 
 class TextColorDecorator implements StyleDecorator
 {
     private ColorDecorator $colorDecorator;
 
-    public function __construct(array $textColors)
+    public function __construct(TextColorConfigurator $configurator)
     {
-        if( empty($textColors)){
+        $colors = $configurator->configure();
+        if( empty($colors)){
             throw new \Exception('Text colors are not defined.');
         }
-        $this->colorDecorator = new ColorDecorator($textColors);
+        $this->colorDecorator = new ColorDecorator($colors);
     }
 
     public function setColor(string $color)
